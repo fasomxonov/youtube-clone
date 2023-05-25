d6410d27fb35d462e7e4fb0c8f8b21b90d1f7cc3
fasomxonov <sayferanonim@gmail.com>
2023-05-25 20:42:36 +0500

    addition search and mobile search components

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -11,8 +11,12 @@
         <LogoMain />
       </div>
     </div>
-    <TheSearchMobile/>
+    <TheSearchMobile
+      v-if="isMobileSearchShown"
+      @close="closeMobileSearch"
+    />
     <div
+      v-else
       class="hidden sm:flex items-center justify-end p-2.5 pl-8 md:pl-12 md:px-8 flex-1 lg:px-0 lg:w-1/2 max-w-screen-md"
     >
       <TheSearch />
@@ -25,16 +29,19 @@
     <div
       class="flex items-center justify-end lg:w-1/4 sm:space-x-3 p-2 sm:px-4"
     >
-    <BaseTooltip text="Search with your voice">
-      <button class="sm:hidden p-2 focus:outline-none">
-        <BaseIcon name="microphone" class="w-5 h-5" />
-      </button>
-    </BaseTooltip>
-    <BaseTooltip text="Search">
-      <button class="sm:hidden p-2 focus:outline-none">
-        <BaseIcon name="search" class="w-5 h-5" />
-      </button>
-    </BaseTooltip>
+      <BaseTooltip text="Search with your voice">
+        <button class="sm:hidden p-2 focus:outline-none">
+          <BaseIcon name="microphone" class="w-5 h-5" />
+        </button>
+      </BaseTooltip>
+      <BaseTooltip text="Search">
+        <button
+          @click.stop="isMobileSearchActive = true"
+          class="sm:hidden p-2 focus:outline-none"
+        >
+          <BaseIcon name="search" class="w-5 h-5" />
+        </button>
+      </BaseTooltip>
       <TheDropdownApps />
       <TheDropdownSettings />
       <ButtonLogin />
@@ -60,10 +67,38 @@ export default {
     TheSearchMobile,
     ButtonLogin,
     BaseIcon,
-    BaseTooltip
+    BaseTooltip,
   },
   emits: {
     toggleSidebar: null,
+  },
+  data() {
+    return {
+      isSmallScreen: false,
+      isMobileSearchActive: false,
+    };
+  },
+  computed: {
+    isMobileSearchShown() {
+      return this.isSmallScreen && this.isMobileSearchActive
+    }
+  },
+  mounted() {
+    this.onResize();
+    window.addEventListener("resize", this.onResize);
+  },
+  methods: {
+    onResize() {
+      if (window.innerWidth < 640) {
+        this.isSmallScreen = true;
+        return;
+      }
+      this.closeMobileSearch();
+      this.isSmallScreen = false;
+    },
+    closeMobileSearch() {
+      this.isMobileSearchActive = false;
+    },
   },
 };
 </script>
